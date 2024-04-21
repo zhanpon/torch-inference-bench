1523547e384eb0fe84eb091c4af9c55e599afb1c
Yiyang Zhan <pon.zhan@gmail.com>
2024-04-21 14:35:11 +0900

    Implement profiling

--- torchvision_bench.py
+++ torchvision_bench.py
@@ -2,9 +2,11 @@ import click
 import torch
 import torch.utils.benchmark as benchmark
 import torchvision
+import torch.profiler as profiler
 
 
 @click.command()
+@click.option("--profile", is_flag=True)
 @click.option("--model-name", default="resnet50")
 @click.option("--iterations", type=int, default=100)
 @click.option("--batch-size", type=int, default=1)
@@ -15,6 +17,7 @@ import torchvision
 @click.option("--jit", is_flag=True)
 @click.option("--compile", "with_compile", is_flag=True)
 def main(
+    profile: bool,
     model_name: str,
     iterations: int,
     batch_size: int,
@@ -51,13 +54,24 @@ def main(
     def do_infer():
         return m(x)
 
-    t = benchmark.Timer(
-        stmt="do_infer()",
-        globals={"do_infer": do_infer},
-        num_threads=num_threads,
-    )
+    if profile:
+        torch.set_num_threads(num_threads)
+        for _ in range(3):
+            do_infer()
 
-    print(t.timeit(iterations))
+        with profiler.profile(activities=[profiler.ProfilerActivity.CPU]) as prof:
+            with profiler.record_function("do_infer"):
+                do_infer()
+
+        print(prof.key_averages().table(sort_by="self_cpu_time_total", row_limit=10))
+    else:
+        t = benchmark.Timer(
+            stmt="do_infer()",
+            globals={"do_infer": do_infer},
+            num_threads=num_threads,
+        )
+
+        print(t.timeit(iterations))
 
 
 if __name__ == "__main__":
